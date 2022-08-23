a232dbb54c2a7cda420b3da213a4186abdf89713
potatoy1 <ekyoung01121@gmail.com>
2022-08-23 18:44:01 +0900

    0823 update

---
+++ 08-4w/[0823] keyword.js
@@ -0,0 +1,29 @@
+var v_jsStudy = [ 
+    //?
+    "use strict","var","Number","String","Boolean","Array []","Object {}","true","false","null","NaN","undefined","infinite","JSON","case",
+    "for","while","switch","continue","break","function","return","if elseif else","&& and","|| or","typeof","instanceof","new","==","===",
+    "trim","indexOf","replace","split","substr","push","splice","sort","this","call","apply","bind","stringify","parse","regexp //","test","$1 $2",
+    "i++","++i", "i +=3","Math","random","ceil","floor","sin","cos","round","FileReader","prototype","readAsDataURL","readAsText","result",
+    //?
+    "window","location","history","navigator","screen","userAgent","opener",
+    "open","close","frames","self","top","innerWidth","innerHeight","href","reload","replace","setTimeout","clearTimeout",
+    //?
+    "document","write","getElementById","getElementsByName","getElementsByTagName","getElementsByClassName","querySelector","querySelectorAll",
+    "styleSheets","cssRules","cssText","style","selectorText","innerHTML","children","parentElement","createElement","setAttribute",
+    "getAttribute","appendChild",
+    //?
+    "onload","onclick","onchange","onmouseover","onmousemove","ondragover","ondrop","ondragstart","onunload","onkeydown","clientX","clientY","key",
+    "onmousedown","onmouseup","onfocus","onblur","stopPropagation","preventDefault","addEventListener","dataTransfer",
+    //?
+    "display","position","z-index","overflow","transform","visibility","background","margin","padding","border","opacity",
+    "block","inline","static","relative","absolute","hidden","auto","visible","width","height","auto","text-align","line-height",
+    "rotate","translate","skew","scale","id #","class .",
+    //?
+    "a","input","img","div","p","h1~h6","pre","span","ul","ol","li","select","option","textarea","form","audio","video","canvas","svg",
+    "table","tr","td",
+    //?
+    "type","name","value","pattern","action","method","required","readonly","disabled","checked","download","target","accept",
+    "rowspan","colspan","rows","cols","data-",
+    //?
+    "button","radio","checkbox","date","color","file","submit","reset"
+];
