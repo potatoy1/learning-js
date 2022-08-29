a4b59a3ff2641719e506018b834598013b89b2ab
potatoy1 <ekyoung01121@gmail.com>
2022-08-29 20:36:52 +0900

    0829 update

--- 07-4w/js/[0728] mylib.js
+++ 07-4w/js/[0728] mylib.js
@@ -1,5 +1,6 @@
- //앞으로도 쓸 만한 건 여기다 가져다 놓을거얌
- //from ? to ? 랜덤 정수를 발생시키는 함수
+ // 앞으로도 쓸 만한 건 여기다 가져다 놓을거얌
+ // from ? to ? 랜덤 정수를 발생시키는 함수
+ // 상수값이나 함수를 주로 정의
 function f_ranNum(p_start, p_end){
     return Math.floor(Math.random()*(p_end-p_start +1)) + p_start;
 }
